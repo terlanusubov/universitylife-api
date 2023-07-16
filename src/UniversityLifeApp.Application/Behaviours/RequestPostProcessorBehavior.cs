90d5e76d51dc51a7d30fdb156afd93799aeb93c4
QEyyub <kadirofff5@outlook.com>
2023-07-16 14:36:43 +0400

    Behaviours

---
+++ src/UniversityLifeApp.Application/Behaviours/RequestPostProcessorBehavior.cs
@@ -0,0 +1,21 @@
+﻿using MediatR.Pipeline;
+using Newtonsoft.Json;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityLifeApp.Application.Behaviours
+{
+    public class RequestPostProcessorBehavior<TRequest, TResponse> : IRequestPostProcessor<TRequest, TResponse>
+    {
+        public async Task Process(TRequest request, TResponse response, CancellationToken cancellationToken)
+        {
+            var responseAsString = JsonConvert.SerializeObject(response);
+            Log.Information("Response is : " + responseAsString);
+            Log.Information("---Request Ended---");
+        }
+    }
+}
