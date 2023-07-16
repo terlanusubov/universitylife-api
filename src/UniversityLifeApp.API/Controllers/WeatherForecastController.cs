a5ce54e096f3d39a604526c018ea0fb5efba5fd4
tarlanusubov <tarlanru@code.edu.az>
2023-07-16 15:40:39 +0400

    deleted gitignore

--- src/UniversityLifeApp.API/Controllers/WeatherForecastController.cs
+++ src/UniversityLifeApp.API/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace UniversityLifeApp.API.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
