5e0e4e3a86041352a55abea9e309caab36c9adb2
Nurlan Məmmədov <nurlan.memmedov8818@gmail.com>
2023-07-16 15:37:10 +0400

    coreFolder

--- src/UniversityLifeApp.Infrastructure/Data/ApplicationContext.cs
+++ src/UniversityLifeApp.Infrastructure/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace UniversityLifeApp.Infrastructure.Data
 {
-    public class ApplicationContext
+    public class ApplicationContext:DbContext
     {
 
     }
