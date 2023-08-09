e1443e9da1a3f49275b35952f55d06c179a25739
“[Elnur <[huseynove174@gmail.com]>
2023-08-09 18:18:46 +0400

    Add validator to bedroomroom api

---
+++ src/UniveristyLifeApp.Models/v1/BedRoomRoom/UpdateBedRoomRoom/UpdateBedRoomRoomResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniveristyLifeApp.Models.v1.BedRoomRoom.UpdateBedRoomRoom
+{
+    public class UpdateBedRoomRoomResponse
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public int BedRoomId { get; set; }
+        public int BedRoomRoomTypeId { get; set; }
+        public int BedRoomRoomStatusId { get; set; }
+    }
+}
---
+++ src/UniversityLifeApp.Application/CQRS/v1/BedRoomRoom/Commands/CreateBedRoomRoom/CreateBedRoomRoomValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityLifeApp.Application.CQRS.v1.BedRoomRoom.Commands.CreateBedRoomRoom
+{
+    public class CreateBedRoomRoomValidator:AbstractValidator<CreateBedRoomRoomCommand>
+    {
+        public CreateBedRoomRoomValidator()
+        {
+            RuleFor(x => x.Request.Name)
+                //.NotNull().WithMessage("The Name field cannot be empty.")
+                .MinimumLength(3).WithMessage("The name must contain at least 3 letters.")
+                .MaximumLength(30).WithMessage("The name can contain up to 30 characters.");
+
+            RuleFor(x => x.Request.Description)
+                //.NotNull().WithMessage("The description field cannot be empty.")
+                .MinimumLength(10).WithMessage("The name must contain at least 10 letters.")
+                .MaximumLength(500).WithMessage("The name can contain up to 500 characters.");
+
+            RuleFor(x => x.Request.Price)
+                .NotNull().WithMessage("The price field cannot be empty.")
+                .GreaterThanOrEqualTo(1).WithMessage("Price cannot be less than £1.");
+
+            RuleFor(x => x.Request.BedRoomId).NotNull();
+            RuleFor(x => x.Request.BedRoomRoomTypeId).NotNull();
+        }
+    }
+}
